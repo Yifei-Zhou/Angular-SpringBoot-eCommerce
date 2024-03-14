a4b34e732181a543ce16b682c6d778276c679663
Yifei-Zhou <919915468@qq.com>
2024-03-14 11:44:14 -0400

    Commit Unstashed Changes

--- source-code/ecommerce-project-2.0-from-start/03-frontend/angular-ecommerce/src/app/app.module.ts
+++ source-code/ecommerce-project-2.0-from-start/03-frontend/angular-ecommerce/src/app/app.module.ts
@@ -8,8 +8,13 @@ import { ProductService } from './services/product.service';
 
 import { Routes, RouterModule} from '@angular/router';
 import { ProductCategoryMenuComponent } from './components/product-category-menu/product-category-menu.component';
+import { SearchComponent } from './components/search/search.component';
+import { ProductDetailsComponent } from './components/product-details/product-details.component';
+import { Product } from './common/product';
 
 const routes: Routes = [
+  {path: 'products/:id', component: ProductDetailsComponent},
+  {path: 'search/:keyword', component: ProductListComponent},
   {path: 'category/:id/:name', component: ProductListComponent},
   {path: 'category', component: ProductListComponent},
   {path: 'products', component: ProductListComponent},
@@ -21,7 +26,9 @@ const routes: Routes = [
   declarations: [
     AppComponent,
     ProductListComponent,
-    ProductCategoryMenuComponent
+    ProductCategoryMenuComponent,
+    SearchComponent,
+    ProductDetailsComponent
   ],
   imports: [
     RouterModule.forRoot(routes),
